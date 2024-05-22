cdcf41b57b679f05021ba70a97ef79ca2575dd33
nishi8899 <66168880+nishi8899@users.noreply.github.com>
2024-05-22 23:38:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Get the MongoDB URI from environment variables
+const db = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+    console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    //Exit process with failure
+    // Exit process with failure
     process.exit(1);
   }
 };
